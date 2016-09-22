524f3fd3ac9539d0c860daa39921549b0f61c8bd
shiro105 <shiro105@mail.chapman.edu>
2016-09-22 14:17:27 -0700

    First commit

---
+++ Assets/Scripts/MouseCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class will control the player camera. It will be a 1st person, mouse controlled.
+
+public class MouseCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class will simply have the player controls. This class will have an interact button. This will do various things with the enviroment.
+
+using System;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;						//How fast is the player moving?
+	public bool isInteracting = false;			//Is the player interacting with anything?
+
+	private CharacterController playerControl;	//The character controller attatched to the player.
+
+	//Makes sure there's only one player in the room.
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	//Assigns the playerController to a private variable.
+	void Start()
+	{
+		playerControl = gameObject.GetComponent<CharacterController>();
+	}
+	
+	//Checks if the player's hit the interact button. Else, the player is moving around.
+	void Update () 
+	{
+		if(isInteracting == false)
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				print("You're interacting!");
+				isInteracting = true;
+				StartCoroutine(NotInteractingAnymore(2f));
+			}
+			else
+			{
+				Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= moveSpeed;
+				playerControl.Move(moveDirection * Time.deltaTime);
+			}
+		}
+	}
+
+	//This is called in order to allow the player to move again.
+	IEnumerator NotInteractingAnymore(float time)
+	{
+		yield return new WaitForSeconds(time);
+		print("You can interact again!");
+		isInteracting = false;
+	}
+}
