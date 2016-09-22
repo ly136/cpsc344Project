13de0f705c07fcac5d7c0b46b400fe891b5a05d8
shiro105 <shiro105@mail.chapman.edu>
2016-09-22 14:38:03 -0700

    Added script when player is near object or interacting with it.

---
+++ Assets/Scripts/ScriptedEvent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This activates an event if the player walks into it, or if the player inspects it within its range.
+
+public class ScriptedEvent : MonoBehaviour {
+
+	public bool isActivatedByInspect = false;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			if(isActivatedByInspect == true && other.gameObject.GetComponent<PlayerMovement>().isInteracting == true)
+			{
+				print("Activate cutscene by inspecting here!");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			if(isActivatedByInspect == false)
+			{
+				print("Activate cutscene by entering here!");
+			}
+		}
+	}
+
+
+}
