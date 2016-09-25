c8b2a17ed4796d65bc30c70d50f5d2d4cb7535a3
shiro105 <shiro105@mail.chapman.edu>
2016-09-25 16:55:01 -0700

    Added MouseCamera and updated PlayerMovement scripts

--- Assets/Scripts/MouseCamera.cs
+++ Assets/Scripts/MouseCamera.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-//This class will control the player camera. It will be a 1st person, mouse controlled.
+// This class will control the player camera. It will be a 1st person, mouse controlled.
+// I used this turotial to make this: https://www.youtube.com/watch?v=3JsuldsGuNw&index=4&list=WL
 
 public class MouseCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float lookSensitivity = 5f;	//Allows for a more fluid movement with thr mouse
+	public float yRotation;				//Target rotations, that we can use that allows for the player to turn at set areas
+	public float xRotation;
+	public float currentYRotation;		//The current rotation that the player is at.
+	public float currentXRotation;
+	public float yRotationV;			//The speed that the rotation is
+	public float xRotationV;
+	public float lookSmoothDamp = 0.1f;		//How smooth the move will be
+
+	void Update()
+	{
+		yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+		xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+		xRotation = Mathf.Clamp(xRotation, - 90,90);
+
+		currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+		currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
+
+		transform.rotation = Quaternion.Euler(currentXRotation,currentYRotation, 0);
 	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//This class will simply have the player controls. This class will have an interact button. This will do various things with the enviroment.
+// This class will simply have the player controls. This class will have an interact button. This will do various things with the enviroment.
+// I used https://www.youtube.com/watch?v=5pkeRlpjFzQ&index=4&list=WL to help fix some bugs!
 
 using System;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float moveSpeed;						//How fast is the player moving?
+	public float walkAcceleleration = 5f;		//How fast will the player accelerate?
+	public float maxWalkSpeed = 10f;			//The max speed the player can move
+	public float maxFallSpeed = 10f;
 	public bool isInteracting = false;			//Is the player interacting with anything?
+	public bool isGrounded;						//Is the player on the floor?
+	public GameObject cameraControl;			//The camera control attatched to the player
 
-	private CharacterController playerControl;	//The character controller attatched to the player.
+	private Rigidbody playerControl;			//The rigidbody controller attatched to the player.
+	private Vector3 horizontalMovement;			//Used to constrain the player's top speed
+	private Vector3 verticalMovement;			//Used to constrain the player's falling speed.
 
 	//Makes sure there's only one player in the room.
 	void Awake()
@@ -21,7 +28,7 @@ public class PlayerMovement : MonoBehaviour {
 	//Assigns the playerController to a private variable.
 	void Start()
 	{
-		playerControl = gameObject.GetComponent<CharacterController>();
+		playerControl = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	//Checks if the player's hit the interact button. Else, the player is moving around.
@@ -37,11 +44,54 @@ public class PlayerMovement : MonoBehaviour {
 			}
 			else
 			{
-				Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
-				moveDirection = transform.TransformDirection(moveDirection);
-				moveDirection *= moveSpeed;
-				playerControl.Move(moveDirection * Time.deltaTime);
+				transform.rotation = Quaternion.Euler(0,cameraControl.GetComponent<MouseCamera>().currentYRotation,0);
+				if(isGrounded == false)
+				{
+					verticalMovement = new Vector3(0,playerControl.velocity.y,0);
+					if(verticalMovement.magnitude > maxFallSpeed)
+					{
+						verticalMovement.Normalize();
+						verticalMovement *= maxFallSpeed;
+						playerControl.velocity = verticalMovement;
+					}
+				}
+				else
+				{
+					if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+					{
+						playerControl.velocity = Vector3.zero;
+					}
+					else
+					{
+						playerControl.AddRelativeForce(Input.GetAxis("Horizontal") * walkAcceleleration, 0, Input.GetAxis("Vertical") * walkAcceleleration);
+						horizontalMovement = new Vector3(playerControl.velocity.x, 0, playerControl.velocity.z);
+						if(horizontalMovement.magnitude > maxWalkSpeed)
+						{
+							horizontalMovement.Normalize();
+							horizontalMovement *= maxWalkSpeed;
+							playerControl.velocity = horizontalMovement;
+						}
+					}
+				}
 			}
+		}
+	}
+
+	//Checks if the player is off the ground
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Ground")
+		{
+			isGrounded = false;
+		}
+	}
+
+	//Checks if the player is on the ground.
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
 		}
 	}
 
