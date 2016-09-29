9ba7beb6a326d3b5f9f24336891705be5834e4e7
shiro105 <shiro105@mail.chapman.edu>
2016-09-29 00:03:20 -0700

    Added Door part to the script

--- Assets/Scripts/ItemGetNeedEvent.cs
+++ Assets/Scripts/ItemGetNeedEvent.cs
@@ -7,33 +7,58 @@ public class ItemGetNeedEvent : MonoBehaviour {
 
 	public string itemAvailableOrNeed;				//What item can the player acquire/use here?
 	public bool isUsingItemEvent;					//Is the player using an item here?
-	public bool currentlyInspectingThis;			//Is the event currenly playing out?
 	public bool hasSolvedEvent;						//Has the event item been gotten or did the player solve it?
+	public bool isDoorEvent;						//Is this a door event?
+
+	private bool isOpen;							//Is the door currently open?
+	private bool isRunning;							//Is the door part running?
 
 	//This determines if the player is inspecting said spot and will proceed to do said action depending on what we hard coded in.
 	void OnTriggerStay(Collider other)
 	{
 		if(other.gameObject.name == "Player")
 		{
-			if(other.gameObject.GetComponent<PlayerMovement>().isInteracting == true && currentlyInspectingThis == false)
+			if(other.gameObject.GetComponent<PlayerMovement>().isInteracting == true)
 			{
-				currentlyInspectingThis = true;
-				if(isUsingItemEvent == true)
+				if(hasSolvedEvent == false)
 				{
-					if(other.gameObject.GetComponent<PlayerInventory>().CheckIfPlayerHasItem(itemAvailableOrNeed) == true)
+					if(isUsingItemEvent == true)
 					{
-						print("You used " + itemAvailableOrNeed);
-						other.gameObject.GetComponent<PlayerInventory>().RemoveFromInventory(itemAvailableOrNeed);
+						if(other.gameObject.GetComponent<PlayerInventory>().CheckIfPlayerHasItem(itemAvailableOrNeed) == true)
+						{
+							print("You used " + itemAvailableOrNeed);
+							other.gameObject.GetComponent<PlayerInventory>().RemoveFromInventory(itemAvailableOrNeed);
+							hasSolvedEvent = true;
+						}
+					}
+					else
+					{
+						other.gameObject.GetComponent<PlayerInventory>().AddToInventory(itemAvailableOrNeed);
 						hasSolvedEvent = true;
 					}
 				}
-				else
+				else if(isDoorEvent == true && isRunning == false)
 				{
-					other.gameObject.GetComponent<PlayerInventory>().AddToInventory(itemAvailableOrNeed);
-					hasSolvedEvent = true;
+					isRunning = true;
+					if(isOpen == true)
+					{
+						//Insert something with animation
+						isOpen = false;
+
+					}
+					else
+					{
+						//Insert something with animation
+						isOpen = true;
+					}
 				}
-				other.gameObject.GetComponent<PlayerMovement>().isInteracting = false;
 			}
+			other.gameObject.GetComponent<PlayerMovement>().isInteracting = false;
 		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		isRunning = false;
 	}
 }
