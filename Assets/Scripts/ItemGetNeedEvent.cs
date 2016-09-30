22e2423ab228c64ba5d2a03d17d9ea87d7fe301d
shiro105 <shiro105@mail.chapman.edu>
2016-09-29 20:53:11 -0700

    Removed door part in script

--- Assets/Scripts/ItemGetNeedEvent.cs
+++ Assets/Scripts/ItemGetNeedEvent.cs
@@ -8,10 +8,6 @@ public class ItemGetNeedEvent : MonoBehaviour {
 	public string itemAvailableOrNeed;				//What item can the player acquire/use here?
 	public bool isUsingItemEvent;					//Is the player using an item here?
 	public bool hasSolvedEvent;						//Has the event item been gotten or did the player solve it?
-	public bool isDoorEvent;						//Is this a door event?
-
-	private bool isOpen;							//Is the door currently open?
-	private bool isRunning;							//Is the door part running?
 
 	//This determines if the player is inspecting said spot and will proceed to do said action depending on what we hard coded in.
 	void OnTriggerStay(Collider other)
@@ -37,28 +33,9 @@ public class ItemGetNeedEvent : MonoBehaviour {
 						hasSolvedEvent = true;
 					}
 				}
-				else if(isDoorEvent == true && isRunning == false)
-				{
-					isRunning = true;
-					if(isOpen == true)
-					{
-						//Insert something with animation
-						isOpen = false;
 
-					}
-					else
-					{
-						//Insert something with animation
-						isOpen = true;
-					}
-				}
 			}
 			other.gameObject.GetComponent<PlayerMovement>().isInteracting = false;
 		}
-	}
-
-	void OnTriggerExit(Collider other)
-	{
-		isRunning = false;
 	}
 }
