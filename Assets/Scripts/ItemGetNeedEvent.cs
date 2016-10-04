ac64c9ac3c6957f0b42d1c072cc137175001a259
shiro105 <shiro105@mail.chapman.edu>
2016-10-03 19:52:09 -0700

    Updated ItemGet so you can put multiple items on an event

--- Assets/Scripts/ItemGetNeedEvent.cs
+++ Assets/Scripts/ItemGetNeedEvent.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 //This event occurs if the player inspects it. The player will be given an item/uses an item if they have it.
+using System.Collections.Generic;
 
 public class ItemGetNeedEvent : MonoBehaviour {
 
-	public string itemAvailableOrNeed;				//What item can the player acquire/use here?
-	public bool isUsingItemEvent;					//Is the player using an item here?
-	public bool hasSolvedEvent;						//Has the event item been gotten or did the player solve it?
+	public List<string> itemAvailableOrNeed = new List<string>();			//What items can the player acquire/use here?
+	public bool isUsingItemEvent;											//Is the player using an item here?
+	public bool hasSolvedEvent;												//Has the event item been gotten or did the player solve it?
 
 	//This determines if the player is inspecting said spot and will proceed to do said action depending on what we hard coded in.
 	void OnTriggerStay(Collider other)
@@ -20,16 +21,21 @@ public class ItemGetNeedEvent : MonoBehaviour {
 				{
 					if(isUsingItemEvent == true)
 					{
-						if(other.gameObject.GetComponent<PlayerInventory>().CheckIfPlayerHasItem(itemAvailableOrNeed) == true)
+						string firstItem = itemAvailableOrNeed[0];
+						if(other.gameObject.GetComponent<PlayerInventory>().CheckIfPlayerHasItem(firstItem) == true)
 						{
 							print("You used " + itemAvailableOrNeed);
-							other.gameObject.GetComponent<PlayerInventory>().RemoveFromInventory(itemAvailableOrNeed);
-							hasSolvedEvent = true;
+							other.gameObject.GetComponent<PlayerInventory>().RemoveFromInventory(firstItem);
+							itemAvailableOrNeed.Remove(firstItem);
+							if(itemAvailableOrNeed.Count == 0)
+							{
+								hasSolvedEvent = true;
+							}
 						}
 					}
 					else
 					{
-						other.gameObject.GetComponent<PlayerInventory>().AddToInventory(itemAvailableOrNeed);
+						other.gameObject.GetComponent<PlayerInventory>().AddToInventory(itemAvailableOrNeed[0]);
 						hasSolvedEvent = true;
 					}
 				}
