6ca91e8dcaa529851ff39bc21c5c9577ff0a6f06
shiro105 <shiro105@mail.chapman.edu>
2016-10-04 13:48:28 -0700

    Allowed player to inspect event with needed items out of order.

--- Assets/Scripts/ItemGetNeedEvent.cs
+++ Assets/Scripts/ItemGetNeedEvent.cs
@@ -1,47 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-//This event occurs if the player inspects it. The player will be given an item/uses an item if they have it.
 using System.Collections.Generic;
 
+//This event occurs if the player inspects it. The player will be given an item/uses an item if they have it.
+//If the player is getting an item, the item list MUST have only one item.
 public class ItemGetNeedEvent : MonoBehaviour {
 
 	public List<string> itemAvailableOrNeed = new List<string>();			//What items can the player acquire/use here?
 	public bool isUsingItemEvent;											//Is the player using an item here?
 	public bool hasSolvedEvent;												//Has the event item been gotten or did the player solve it?
+	public bool destroyOnComplete;											//Will this object get removed once it's cleared?
 
-	//This determines if the player is inspecting said spot and will proceed to do said action depending on what we hard coded in.
+	//Checks whether this event will be destroyed once it's complete.
+	void Update()
+	{
+		if(hasSolvedEvent == true && destroyOnComplete == true)
+			Destroy(gameObject);
+	}
+
+	//This determines if the player is inspecting said spot and will either pick up an item or use an item.
 	void OnTriggerStay(Collider other)
 	{
 		if(other.gameObject.name == "Player")
 		{
-			if(other.gameObject.GetComponent<PlayerMovement>().isInteracting == true)
+			PlayerActions player = other.gameObject.GetComponent<PlayerActions>();
+			if(player.isInteracting == true)
 			{
 				if(hasSolvedEvent == false)
 				{
 					if(isUsingItemEvent == true)
 					{
-						string firstItem = itemAvailableOrNeed[0];
-						if(other.gameObject.GetComponent<PlayerInventory>().CheckIfPlayerHasItem(firstItem) == true)
+						//We look through the player's inventory to see if this event has an item that the player can use. If the player has multiple items
+						for(int i = 0; i < player.GetInventoryLength(); i++)
 						{
-							print("You used " + itemAvailableOrNeed);
-							other.gameObject.GetComponent<PlayerInventory>().RemoveFromInventory(firstItem);
-							itemAvailableOrNeed.Remove(firstItem);
-							if(itemAvailableOrNeed.Count == 0)
+							string currItem = player.GetItem(i);
+							if(itemAvailableOrNeed.Contains(currItem) == true)
 							{
-								hasSolvedEvent = true;
+								print("You used " + currItem);
+								player.RemoveFromInventory(currItem);
+								itemAvailableOrNeed.Remove(currItem);
+								if(itemAvailableOrNeed.Count == 0)
+									hasSolvedEvent = true;
+								break;
 							}
 						}
 					}
 					else
 					{
-						other.gameObject.GetComponent<PlayerInventory>().AddToInventory(itemAvailableOrNeed[0]);
+						player.AddToInventory(itemAvailableOrNeed[0]);
 						hasSolvedEvent = true;
 					}
 				}
-
 			}
-			other.gameObject.GetComponent<PlayerMovement>().isInteracting = false;
+			player.isInteracting = false;
 		}
 	}
 }
