677e65bcbf2cbd4a0cffd3583049ed5796a00478
shiro105 <shiro105@mail.chapman.edu>
2016-10-05 14:06:59 -0700

    Added SwitchInspect and SwitchReceive

---
+++ Assets/Scripts/SwitchInspect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This class will be used to see if inspecting this object will trigger a switch.
+public class SwitchInspect : MonoBehaviour {
+
+	public bool isSwitchActivated;		//Is the switch turned on?
+
+	// This checks if the player has inspected the object to turn on/off the switch.
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			if(other.gameObject.GetComponent<PlayerActions>().isInteracting == true)
+			{
+				if(isSwitchActivated == false)
+				{
+					isSwitchActivated = true;
+					print("I'm activated!");
+				}
+				else
+				{
+					isSwitchActivated = false;
+					print("I'm deactivated!");
+				}
+				GameObject.FindGameObjectWithTag("SwitchDoor").transform.GetChild(1).GetComponent<SwitchReceiver>().CheckSwitchObjects();
+			}
+			other.gameObject.GetComponent<PlayerActions>().isInteracting = false;
+		}
+	}
+}
---
+++ Assets/Scripts/SwitchReceiver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This object takes all of the switch objects that it's associated with an checks if all of them are activated.
+public class SwitchReceiver : MonoBehaviour {
+
+	public GameObject[] switchInspectObjects;	//Array of all of the switch objects that this event is associated with.
+	public bool hasSolvedEvent;					//Is this event solved?
+
+	//If all of the switch objects have been activated, this event is solved.
+	public void CheckSwitchObjects()
+	{
+		for(int i = 0; i < switchInspectObjects.Length; i++)
+		{
+			if(switchInspectObjects[i].GetComponent<SwitchInspect>().isSwitchActivated == false)
+			{
+				hasSolvedEvent = false;
+				print("I'm closed...");
+				return;
+			}
+				
+		}
+		hasSolvedEvent = true;
+		print("I'm open!");
+	}
+
+}
