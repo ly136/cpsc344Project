a7392bd850e7034a349a7d80b73c1b6e4634c548
shiro105 <shiro105@mail.chapman.edu>
2016-11-07 22:23:17 -0800

    Added particle effect to inspect events

---
+++ Assets/Scripts/EventScripts/ActivateParticles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script activates whether not the particle system for the particular object can be used.
+// The gameObject needs to be attatched to the gameObject that has the event where the player can interact with.
+public class ActivateParticles : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+			gameObject.transform.GetChild(0).gameObject.SetActive(true);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+			gameObject.transform.GetChild(0).gameObject.SetActive(false);
+	}
+}
--- Assets/Scripts/EventScripts/SwitchInspect.cs
+++ Assets/Scripts/EventScripts/SwitchInspect.cs
@@ -7,6 +7,20 @@ public class SwitchInspect : MonoBehaviour {
 	public GameObject switchReceiver;	//Which Gameobject are these activating towards?
 	public bool isSwitchActivated;		//Is the switch turned on?
 
+	// Takes the particle emitter from the child and activates it when the player approaches this
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+			gameObject.transform.GetChild(0).gameObject.SetActive(true);
+	}
+
+	// When the player steps away from this object, the child  will stop emitting particles
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+			gameObject.transform.GetChild(0).gameObject.SetActive(false);
+	}
+
 	// This checks if the player has inspected the object to turn on/off the switch.
 	void OnTriggerStay(Collider other)
 	{
