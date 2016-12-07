bb85f29a2111319f392973ffdcf3bcfd6bd9e13b
SleepingArtichoke <crackbone17@gmail.com>
2016-12-07 02:41:57 -0800

    Finished More Rooms
    Clean Bedroom: Done
    
    Clean Living Room: Fix Fire particle effect. Fix fireplace not opening.
    
    Messy Bedroom: Lever needs to be fixed. Script erroring out
    
    Messy Living Room: Fix fire particle effect
    
    Hospital: texture maze if I have time. Change color of currently
    textured rooms to look better. Make remote not just a black box.
    Newspaper Model
    
    Mask Room: Add reall door. Make the mask model. Retexture/Recolor if
    time.
    
    Hub: Doors need to have color and/or texture changed in order to
    differentiate them. The doors also could use a script to play the opening
    animation when activated.

---
+++ Assets/Scripts/EventScripts/DoorCloseScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorCloseScript : MonoBehaviour {
+	public GameObject Door;
+
+	void OnTriggerEnter(Collider Other)
+	{ 
+		if(Other.gameObject.tag == "Player")
+		{
+			gameObject.GetComponent<Animation>().Play("Closing");
+
+		}
+}
+}
